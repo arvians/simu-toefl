030732adee64eeeffffaeb0e4024c399b9ce9199
arvians <yanuar.arviansyah@gmail.com>
2020-02-05 17:52:22 +0700

    admin page

---
+++ application/controllers/Admin_view.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_view extends CI_Controller {
+
+    function __construct()
+	{
+		parent::__construct();
+    }
+    
+    public function index(){
+		$data['judul'] = "Admin || Home";
+	
+		$this->load->view('admin/master/header_admin',$data);
+		$this->load->view('admin/master/navbar_admin',$data);
+		$this->load->view('admin/master/sidebar_admin',$data);
+		$this->load->view('admin/home_admin',$data);
+		$this->load->view('admin/master/footer_admin',$data);
+	}
+}
